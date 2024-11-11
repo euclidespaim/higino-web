9bceb733c50bf559c349faedcafebff3d77351f3
euclidespaim <euclidespaim@gmail.com>
2024-11-11 16:45:57 -0300

    menu hamburguer instalado

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // Obtém o ano atual e insere no rodapé
 document.getElementById('current-year').textContent = new Date().getFullYear();
+
+// Função para alternar a visibilidade do menu
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active'); // Alterna a visibilidade do menu
+}
